1d837fba7aaf3589f232aaebe089689915af718b
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-05-14 21:00:43 +0200

    small fix of Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,21 +4,22 @@ export default function Meaning(props) {
     console.log(props.meaning);
     return (
         <>
-            <div> className</div>
-            <h3>{props.meaning.partOfSpeech}</h3>;
-            {props.meaning.definitions.map(function (definition, index) {
-                return (
-                    <div key={index}>
-                        <p>
-                            {definition.definition}
-                            <br />
-                            <em>{definition.example}</em>
-                        </p>
-                    </div>
-                );
-            })}
-            <p>{props.meaning.definitions[0].definition}</p>
-            <p>{props.meaning.definitions[0].example}</p>
+            <div className="Meaning">
+                <h3>{props.meaning.partOfSpeech}</h3>;
+                {props.meaning.definitions.map(function (definition, index) {
+                    return (
+                        <div key={index}>
+                            <p>
+                                {definition.definition}
+                                <br />
+                                <em>{definition.example}</em>
+                            </p>
+                        </div>
+                    );
+                })}
+                <p>{props.meaning.definitions[0].definition}</p>
+                <p>{props.meaning.definitions[0].example}</p>
+            </div>
         </>
     );
 }
